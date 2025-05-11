db74d3ba98ac2a0fb6cf18a37038cf472c5dc56e
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-05-11 15:11:53 +0530

    Added Default Spring Backend Folder

---
+++ backend/src/test/java/com/callmonitoring/CallmonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallmonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
